e2e77f0db9cb8b114b24e49dbc4cccab4b6209d7
Khattiya <Khattiya.b.game@gmail.com>
2023-04-23 21:39:10 +0700

    database fix

--- server.js
+++ server.js
@@ -4,33 +4,30 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const todoListRoutes = require('./routes/todoList');
-// const inventoryRoutes = require('./routes/inventory');
-// const itemRoutes = require('./routes/item');
-// const userRoutes = require('./routes/user');
-// const db = require('./models');
+const inventoryRoutes = require('./routes/inventory');
+const itemRoutes = require('./routes/item');
+const userRoutes = require('./routes/user');
+const db = require('./models');
 const PORT = process.env.PORT;
 
 
 
 
 
-// require('./config/passport/passport');
+require('./config/passport/passport');
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/test', todoListRoutes);  
-// app.use('/inventory', inventoryRoutes);
-// app.use('/item', itemRoutes);
-// app.use('/users', userRoutes);
-// app.use('/image', express.static('./image'));
+app.use('/inventory', inventoryRoutes);
+app.use('/item', itemRoutes);
+app.use('/users', userRoutes);
+app.use('/image', express.static('./image'));
 
-app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
+db.sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running at port ${PORT}`);
+    });
 });
-// db.sequelize.sync({force: false}).then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server is running at port ${PORT}`);
-//     });
-// });
